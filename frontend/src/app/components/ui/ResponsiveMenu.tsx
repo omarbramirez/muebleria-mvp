347a14902093a5427ce4d84ebb10f95cb62bccad
omarbramirez <omar.ramirez94@hotmail.es>
2025-09-06 09:05:56 -0600

    window and document bugs fixed

--- frontend/src/app/components/ui/ResponsiveMenu.tsx
+++ frontend/src/app/components/ui/ResponsiveMenu.tsx
@@ -1,14 +1,18 @@
 "use client";
 import React, { useEffect, useRef } from "react";
+
 import { useTranslations } from "next-intl";
 import { LinkItem } from "@/app/components/ui/LinkItem";
-import { ResponsiveMenuProps } from "@/types/index";
+import {ResponsiveMenuProps } from '@/types/index';
+
 
 export const ResponsiveMenu = ({ isOpen, setIsOpen }: ResponsiveMenuProps) => {
   const t = useTranslations("navbar");
+  
+ // 1. Ref para el contenedor
   const menuRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       const target = event.target as HTMLDivElement;
       if (isOpen && menuRef.current && !menuRef.current.contains(target)) {
@@ -16,24 +20,32 @@ export const ResponsiveMenu = ({ isOpen, setIsOpen }: ResponsiveMenuProps) => {
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    window.addEventListener("scroll", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      window.removeEventListener("scroll", handleClickOutside);
+    const handleScroll = () => {
+      setIsOpen(false);
     };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    window.addEventListener("scroll", handleScroll);
+
+    // return () => {
+    //   document.removeEventListener("mousedown", handleClickOutside);
+    //   window.removeEventListener("scroll", handleScroll);
+    // };
   }, [isOpen, setIsOpen]);
 
+
+
+
   return (
-    <nav ref={menuRef} className="md:hidden z-50">
+    <div ref={menuRef} className="w-auto md:hidden z-50" onClick={() => setIsOpen(false)}>
       {isOpen && (
-        <ul className="absolute top-16 left-0 w-full bg-background-light shadow-lg flex flex-col gap-4 p-6">
-          <li><LinkItem href="#top" onClick={() => setIsOpen(false)}>{t("home")}</LinkItem></li>
-          <li><LinkItem href="/#how" onClick={() => setIsOpen(false)}>{t("how_it_works")}</LinkItem></li>
-          <li><LinkItem href="/#catalog" onClick={() => setIsOpen(false)}>{t("catalog")}</LinkItem></li>
-          <li><LinkItem variant="secondary" href="/#reserve" onClick={() => setIsOpen(false)}>{t("reserve")}</LinkItem></li>
+        <ul className="w-auto absolute top-16 left-0 w-full bg-background-light shadow-lg flex flex-col gap-4 p-6" >
+          <li><LinkItem href="#top" >{t("home")}</LinkItem></li>
+          <li><LinkItem href="#how">{t("how_it_works")}</LinkItem></li>
+          <li><LinkItem href="#catalog">{t("catalog")}</LinkItem></li>
+          <li><LinkItem variant="secondary" href="#reserve">{t("reserve")}</LinkItem></li>
         </ul>
       )}
-    </nav>
+    </div>
   );
 };
