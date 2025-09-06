259907a7a4660981122f23b8ea67f17541fef026
omarbramirez <omar.ramirez94@hotmail.es>
2025-09-06 07:48:35 -0600

    href type corrected

--- frontend/src/types/ui/linkitem.ts
+++ frontend/src/types/ui/linkitem.ts
@@ -4,7 +4,9 @@ import { Link } from '@/i18n/navigation';
 
 export type LinkVariant = 'primary' | 'secondary' | 'muted';
 
-export interface LinkItemProps extends ComponentProps<typeof Link> {
+export interface LinkItemProps
+  extends Omit<ComponentProps<typeof Link>, "href"> {
+  href: string; // <- forzamos a string
   variant?: LinkVariant;
   size?: Size;
   className?: string;
