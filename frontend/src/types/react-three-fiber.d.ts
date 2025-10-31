5ef680c08c338ac18787d0c086383254b11886fe
omarbramirez <omar.ramirez94@hotmail.es>
2025-10-31 15:39:32 -0600

    typing fixed

--- frontend/src/types/react-three-fiber.d.ts
+++ frontend/src/types/react-three-fiber.d.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { ReactThreeFiber } from "@react-three/fiber";
-import * as THREE from "three";
 
 declare global {
   namespace JSX {
